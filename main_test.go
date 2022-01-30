cc33c77315b3ba66a4c13aa97e03ce1ef07e3054
mikana0918 <shogo.kusuhara@gmail.com>
2022-01-30 20:46:39 +0900

    test: :gun: add test for http handler

--- main_test.go
+++ main_test.go
@@ -50,3 +50,21 @@ func TestPostAlbums(t *testing.T) {
 		}
 	}
 }
+
+func TestGetAlbumById(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+
+	req, _ := http.NewRequest("GET", "/albums/1", nil)
+
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+
+	req2, _ := http.NewRequest("GET", "/albums/9999999", nil) //should return NotFound
+
+	router.ServeHTTP(w, req2)
+
+	assert.Equal(t, 404, w.Code)
+}
