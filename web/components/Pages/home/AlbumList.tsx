55a5dff2573ac699f3e3b5b14d06da33e7d3dc7d
mikana0918 <shogo.kusuhara@gmail.com>
2022-02-03 01:14:27 +0900

    refactor: recycle: fix component directory

---
+++ web/components/Pages/home/AlbumList.tsx
@@ -0,0 +1,25 @@
+import Box from '@mui/material/Box';
+import MediaCard from '@/components/Base/Card/MediaCard'
+import { Album } from '@/types'
+
+export const AlbumList = ({ albums }: { albums: Album[] }) => {
+  const list = albums.map(
+    (album, idx) => (
+      <Box mb={4} >
+        <MediaCard
+          key={idx}
+          title={album.Title}
+          description={album.Title}
+          imageSrc={album.ImageSrc}
+          price={album.Price}
+        />
+      </Box>
+    )
+  )
+
+  return (
+    <div>
+      {list}
+    </div>
+  )
+}
