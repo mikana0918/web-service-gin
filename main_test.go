4bd4773a48f3a2b07cc64d9b94629bfcf1e99767
mikana0918 <shogo.kusuhara@gmail.com>
2022-02-08 01:44:29 +0900

    test: :gun: add test for post album

--- main_test.go
+++ main_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/go-playground/assert/v2"
+	"github.com/google/uuid"
 )
 
 func TestGetAlbums(t *testing.T) {
@@ -47,6 +48,43 @@ func TestPostAlbums(t *testing.T) {
 			assert.Equal(t, alb.Title, testAlbum.Title)
 			assert.Equal(t, alb.Artist, testAlbum.Artist)
 			assert.Equal(t, alb.Price, testAlbum.Price)
+		}
+	}
+}
+
+func TestPostAlbumsWithoutId(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+
+	testAlbum := album{
+		ID:     "",
+		Title:  "hoge-title--without-id",
+		Artist: "hoge-artist--without-id",
+		Price:  float64(30.11),
+	}
+
+	testAlbumJson, _ := json.Marshal(testAlbum)
+
+	req, _ := http.NewRequest("POST", "/api/albums", bytes.NewBuffer(
+		testAlbumJson,
+	))
+
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 201, w.Code)
+
+	isValidUuid := func(u string) bool {
+		_, err := uuid.Parse(u)
+		return err == nil
+	}
+
+	for _, alb := range albums {
+		if alb.Title == testAlbum.Title {
+			assert.Equal(t, alb.Title, testAlbum.Title)
+			assert.Equal(t, alb.Artist, testAlbum.Artist)
+			assert.Equal(t, alb.Price, testAlbum.Price)
+			assert.Equal(t, isValidUuid(alb.ID), true)
 		}
 	}
 }
